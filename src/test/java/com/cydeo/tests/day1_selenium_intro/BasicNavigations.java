540f78b1db43769f91428d226acffe0733ce4a4e
gurhank <gurhankocyigit@gmail.com>
2022-02-01 14:16:05 -0600

    day1 basic navigations

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -21,11 +21,23 @@ public class BasicNavigations {
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
 
+
         // use selenium to navigate back
         driver.navigate().back();
 
-        //break until 2.08 pm cst
-        //break until 3.08 pm est
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate forward
+        driver.navigate().forward();
+
+        //Stop code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate refresh
+        driver.navigate().refresh();
+
+
 
     }
 
