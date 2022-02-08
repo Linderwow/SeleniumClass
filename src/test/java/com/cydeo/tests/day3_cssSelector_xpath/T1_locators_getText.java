c5101aca94db3567a65ed20e6c0379517d8ae357
gurhank <gurhankocyigit@gmail.com>
2022-02-08 10:39:03 -0600

    day3 Task1_locators_getText

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -23,9 +23,17 @@ public class T1_locators_getText {
         WebElement inputUsername = driver.findElement(By.className("login-inp"));
         inputUsername.sendKeys("incorrect");
 
-
         //4- Enter incorrect password: “incorrect”
+        //To auto generate local variable
+        //Mac: option + enter
+        //Windows: alt + enter
+        WebElement inputPassword = driver.findElement(By.name("USER_PASSWORD"));
+        inputPassword.sendKeys("incorrect");
+
         //5- Click to log in button.
+        WebElement loginButton = driver.findElement(By.className("login-btn"));
+        loginButton.click();
+
         //6- Verify error message text is as expected:
         //Expected: Incorrect login or password
     }
