355d1646b21c4fa3d1b204f431f85fe1d6baaf2b
gurhank <gurhankocyigit@gmail.com>
2022-02-11 14:52:30 -0600

    day4 Task5 checkboxes

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
@@ -26,7 +26,7 @@ public class T5_checkboxes {
         System.out.println("checkbox1.isSelected(), expecting false = " + checkbox1.isSelected());
 
         //3. Confirm checkbox #2 is SELECTED by default.
-        System.out.println("checkbox2.isSelected(), expecting = true " + checkbox2.isSelected());
+        System.out.println("checkbox2.isSelected(), expecting true = " + checkbox2.isSelected());
 
         //4. Click checkbox #1 to select it.
         Thread.sleep(2000);
@@ -37,7 +37,10 @@ public class T5_checkboxes {
         checkbox2.click();
 
         //6. Confirm checkbox #1 is SELECTED.
+        System.out.println("checkbox1.isSelected(), expecting true = " + checkbox1.isSelected());
+
         //7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox2.isSelected(), expecting false = " + checkbox2.isSelected());
 
 
     }
