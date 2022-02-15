ed0dcbef19457b87103f45d08c332da848d4c2f5
gurhank <gurhankocyigit@gmail.com>
2022-02-15 11:21:16 -0600

    day5 Task1 stale element reference exception practice

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,8 +18,20 @@ public class T2_RadioButton {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //2. Go to https://practice.cydeo.com/radio_buttons
         driver.get("https://practice.cydeo.com/radio_buttons");
+
         //3. Click to “Hockey” radio button
+        WebElement hockeyRadioBtn = driver.findElement(By.xpath("//input[@id='hockey']"));
+
+        hockeyRadioBtn.click();
+
         //4. Verify “Hockey” radio button is selected after clicking.
+        if (hockeyRadioBtn.isSelected()){
+
+            System.out.println("Button is selected. Verification PASSED!");
+
+        }else{
+            System.out.println("Button is not selected. Verification FAILED!!!");
+        }
 
     }
 
