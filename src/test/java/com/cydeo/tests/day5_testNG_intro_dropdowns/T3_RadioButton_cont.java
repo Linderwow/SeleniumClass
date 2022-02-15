8954d9e85099735beca3a976a566c510ce73ac1a
gurhank <gurhankocyigit@gmail.com>
2022-02-15 12:03:42 -0600

    day5 Task1 stale element reference exception practice

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class T3_RadioButton_cont {
 
     public static void main(String[] args) {
+
         //TC #2: Radiobutton handling
         //1. Open Chrome browser
         WebDriver driver = WebDriverFactory.getDriver("chrome");
@@ -20,22 +21,25 @@ public class T3_RadioButton_cont {
         driver.get("https://practice.cydeo.com/radio_buttons");
 
         //Locate name='sports' radio buttons and store them in a List of Web Element
-        List<WebElement> sportRadioButtons = driver.findElements(By.name("sport"));
+
+
+    }
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameAttribute, String idValue){
+        List<WebElement> radioButtons = driver.findElements(By.name(nameAttribute));
 
         //Loop through the List of WebElement and select matching result "hockey"
-        for (WebElement each : sportRadioButtons) {
-
+        for (WebElement each : radioButtons) {
             String eachId = each.getAttribute("id");
 
-            System.out.println("eachId = " + eachId);
-
-            if (eachId.equals("hockey")){
+            if (eachId.equals(idValue)){
 
                 each.click();
-                System.out.println("Hockey is selected : " + each.isSelected());
+                System.out.println( eachId + " is selected : " + each.isSelected());
                 break;
-
             }
         }
+
     }
+
 }
