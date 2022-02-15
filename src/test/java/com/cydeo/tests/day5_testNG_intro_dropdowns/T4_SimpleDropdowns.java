b3e497d5ba6c330d4b224b72e746e8cd73827d3c
gurhank <gurhankocyigit@gmail.com>
2022-02-15 15:36:23 -0600

    day5 Task4 simple dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -9,10 +11,12 @@ import java.util.concurrent.TimeUnit;
 
 public class T4_SimpleDropdowns {
 
+    WebDriver driver;
+
     @BeforeMethod
     public void setupMethod(){
         //1. Open Chrome browser
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -23,8 +27,11 @@ public class T4_SimpleDropdowns {
     @Test
     public void simpleDropdownTest(){
 
-
         //3. Verify “Simple dropdown” default selected value is correct
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+
+
         //Expected: “Please select an option”
         //4. Verify “State selection” default selected value is correct
         //Expected: “Select a State”
