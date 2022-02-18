a19bfca7530517ff88c0a62cc23e740290dd87d5
gurhank <gurhankocyigit@gmail.com>
2022-02-18 13:26:00 -0600

    day6 T1 ALERT PRACTICE 1

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -80,6 +81,11 @@ public class DropdownPractices {
         Assert.assertEquals(actualMonth, expectedMonth);
         Assert.assertEquals(actualDay, expectedDay);
 
+    }
+
+    @AfterMethod
+    public void teardownMethod(){
+        driver.close();
     }
 
 }
---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_Alert_Practices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to website: https://practice.cydeo.com/javascript_alerts
+        driver.get("https://practice.cydeo.com/javascript_alerts");
+    }
+
+    @Test
+    public void alert_test1(){
+
+    }
+
+
+}
