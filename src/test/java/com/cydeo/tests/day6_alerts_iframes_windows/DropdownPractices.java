c06a33f1269bf53fc21055dfffa94e9772c89b1f
gurhank <gurhankocyigit@gmail.com>
2022-02-18 10:44:34 -0600

    day6 Task6 date dropdowns

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -52,9 +52,9 @@ public class DropdownPractices {
     @Test
     public void dropdown_task6(){
         //Select “December 1 , 1923” and verify it is selected.
-        Select yearDropdown = new Select(driver.findElement(By.xpath("")));
-        Select monthDropdown = new Select(driver.findElement(By.xpath("")));
-        Select dayDropdown = new Select(driver.findElement(By.xpath("")));
+        Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='day']")));
 
 
         //Select year using : visible text
