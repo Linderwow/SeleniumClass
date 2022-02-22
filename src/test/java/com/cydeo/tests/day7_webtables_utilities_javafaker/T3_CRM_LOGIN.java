09b62f68ef834818966096a983a10c2bd3ed33d0
gurhank <gurhankocyigit@gmail.com>
2022-02-22 15:15:04 -0600

    day7 T3_CRM_LOGIN

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -30,12 +30,19 @@ public class T3_CRM_LOGIN {
 
         //3. Enter valid username
         WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
+
+        //helpdesk1@cybertekschool.com  UserUser
+        //Helpdesk2@cybertekschool.com  UserUser
 
         //4. Enter valid password
         WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
-
+        inputPassword.sendKeys("UserUser");
 
         //5. Click to Log In button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
         //6. Verify title is as expected:
         //Expected: Portal
     }
