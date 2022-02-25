2cbfa4e7310045271a62cdafb2d1442d233f44a9
gurhank <gurhankocyigit@gmail.com>
2022-02-25 15:46:01 -0600

    day8 T4_Config_Practice

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
+import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -18,7 +19,10 @@ public class T4_Config_Practice {
     @BeforeMethod
     public void setupMethod(){
 
-        driver = WebDriverFactory.getDriver("chrome");
+        //We are getting the browserType dynamically from our configuration.properties file
+        String browserType = ConfigurationReader.getProperty("browser");
+        driver = WebDriverFactory.getDriver(browserType);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.google.com");
@@ -38,8 +42,6 @@ public class T4_Config_Practice {
         String actualTitle = driver.getTitle();
 
         Assert.assertEquals(actualTitle, expectedTitle);
-
-
     }
 
 }
