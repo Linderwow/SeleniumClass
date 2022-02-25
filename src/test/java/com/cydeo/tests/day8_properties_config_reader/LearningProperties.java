443e7df7b04fef3906a8d351630f89519caaf4ed
gurhank <gurhankocyigit@gmail.com>
2022-02-25 14:34:31 -0600

    day8 ReadingProperties

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
@@ -6,8 +6,11 @@ public class LearningProperties {
 
     @Test
     public void java_properties_reading_test(){
-
+        //key = value
+        //os.name = Mac OS X
+        //user.name = cybertekchicago-1
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
 
     }
 
