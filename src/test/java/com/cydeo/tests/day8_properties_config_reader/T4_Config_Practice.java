fd22a964baac46d23c26d33aec43dff54a15d176
gurhank <gurhankocyigit@gmail.com>
2022-02-25 15:34:25 -0600

    day8 T4_Config_Practice

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -23,7 +25,11 @@ public class T4_Config_Practice {
 
     @Test
     public void google_search_test(){
+        //3- Write “apple” in search box
+        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
 
+        //4- Verify title:
+        //Expected: apple - Google Search
     }
 
 }
