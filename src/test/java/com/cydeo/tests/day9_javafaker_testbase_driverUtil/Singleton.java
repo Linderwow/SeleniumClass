3de7bb4221a0860238272ac741c7c6f7d61cf2ba
gurhank <gurhankocyigit@gmail.com>
2022-03-04 15:19:29 -0600

    day9 Singleton.java

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/Singleton.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+public class Singleton {
+
+    //#1- create private constructor
+    private Singleton(){}
+
+    //#2- create private static String
+    // prevent access and provide a getter method
+    private static String word;
+
+
+
+
+}
