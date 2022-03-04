3b1a1f239bc350f7e67aff2bb98390c18c71cac3
gurhank <gurhankocyigit@gmail.com>
2022-03-04 15:48:40 -0600

    day9 SingletonPractice.java

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class SingletonPractice {
 
-    @Test
+    @Test (priority = 2)
     public void singleton_understand_test1(){
 
         String str1 = Singleton.getWord();
@@ -18,7 +18,7 @@ public class SingletonPractice {
 
     }
 
-    @Test
+    @Test (priority = 1)
     public void singleton_understanding_test2(){
         String str4 = Singleton.getWord();
         System.out.println("str4 = " + str4);
