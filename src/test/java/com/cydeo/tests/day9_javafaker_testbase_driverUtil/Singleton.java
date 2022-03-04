338b6909f417eb5ca189177b5f6310dbb60e3be9
gurhank <gurhankocyigit@gmail.com>
2022-03-04 15:22:12 -0600

    day9 Singleton.java

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/Singleton.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/Singleton.java
@@ -9,7 +9,16 @@ public class Singleton {
     // prevent access and provide a getter method
     private static String word;
 
+    //This utility method will return the "word" in the way we want to return
+    public static String getWord(){
 
+        if (word == null){
+            System.out.println("First time call. Word object is null. " +
+                    "Assigning value to it now!");
+            word = "something";
+        }
+
+    }
 
 
 }
