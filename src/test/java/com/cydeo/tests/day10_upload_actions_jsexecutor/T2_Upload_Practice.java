cba1ca39ea194b427eb1a01766c7228bbd697dbb
gurhank <gurhankocyigit@gmail.com>
2022-03-08 13:33:26 -0600

    day10 T2_Upload_Practice.java

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T2_Upload_Practice {
@@ -12,7 +14,12 @@ public class T2_Upload_Practice {
         Driver.getDriver().get("https://practice.cydeo.com/upload");
 
         //2. Find some small file from your computer, and get the path of it.
+        String path = "/Users/cybertekchicago-1/Desktop/HTML Class/cybertruck.jpg";
+
         //3. Upload the file.
+        WebElement fileUpload = Driver.getDriver().findElement(By.id("file-upload"));
+
+
         //4. Assert:
         //-File uploaded text is displayed on the page
     }
