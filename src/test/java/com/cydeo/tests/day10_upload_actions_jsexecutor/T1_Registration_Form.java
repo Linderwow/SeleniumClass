4e603d57660cc1006a9913095a861bb0af342cbe
gurhank <gurhankocyigit@gmail.com>
2022-03-08 12:00:55 -0600

    day10 T1_Registration_Form.java

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T1_Registration_Form.java
@@ -5,6 +5,7 @@ import com.cydeo.utilities.Driver;
 import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class T1_Registration_Form {
@@ -50,8 +51,17 @@ public class T1_Registration_Form {
         inputPhone.sendKeys(faker.numerify("###-###-####"));
 
         //9. Select a gender from radio buttons
+        WebElement inputGender = Driver.getDriver().findElement(By.xpath("//input[@value='female']"));
+        inputGender.click();
+
         //10.Enter date of birth
+        WebElement inputBirthday = Driver.getDriver().findElement(By.xpath("//input[@name='birthday']"));
+        //faker.number().numberBetween(2004, 1920)
+        inputBirthday.sendKeys("03/08/2000");
+
         //11.Select Department/Office
+        Select departmentDropdown = new Select(Driver.getDriver().findElement(By.xpath("//select[@name='department']")));
+
         //12.Select Job Title
         //13.Select programming language from checkboxes
         //14.Click to sign up button
