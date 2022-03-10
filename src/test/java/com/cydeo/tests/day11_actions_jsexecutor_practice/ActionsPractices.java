ad5ec83121e12c71fd8c63fa80f345a7ece4de2c
gurhank <gurhankocyigit@gmail.com>
2022-03-10 10:20:56 -0600

    day11 ActionsPractices.java

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -28,6 +29,8 @@ public class ActionsPractices {
         //4- Scroll using Actions class “moveTo(element)” method
         actions.moveToElement(cydeoLink).perform();
 
+        //5- Scroll back up to “Home” link using PageUP button
+        actions.sendKeys(Keys.PAGE_UP, Keys.PAGE_UP).perform();
 
     }
 }
